3e33ea70e4603f1f1beb010297fd559798253783
jw7914 <jw7914@nyu.edu>
2025-09-24 20:31:27 -0400

    reworked routing logic

--- src/mail_client_service/src/mail_client_service/_impl.py
+++ src/mail_client_service/src/mail_client_service/_impl.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 import gmail_client_impl
 import gmail_message_impl
 import mail_client_api
-client = mail_client_api.get_client(interactive=True)
-print("\nSuccessfully authenticated and connected to the Gmail API.")
+from fastapi.responses import RedirectResponse
 
-# Test 1: Get messages (existing functionality)
-print("\n=== TEST 1: Fetching Messages ===")
-client_messages = list(client.get_messages(max_results=3))
+client = None
 app = FastAPI()
 
 
@@ -15,9 +12,23 @@ app = FastAPI()
 def root():
     return {"message": "Hello World"}
 
+@app.get("/login")
+def login():
+    global client
+    if client:
+        return RedirectResponse(url="/messages")
+    client = mail_client_api.get_client(interactive=True)
+    print("\nSuccessfully authenticated and connected to the Gmail API.")
+
 @app.get("/messages")
 def get_messages():
     # return {"a":"a"}
+    # Test 1: Get messages (existing functionality)
+    global client
+    if not client:
+        return {"Message": "Please login"}
+    print("\n=== TEST 1: Fetching Messages ===")
+    client_messages = list(client.get_messages(max_results=3))
     
     return client_messages
 
