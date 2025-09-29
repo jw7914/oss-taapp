d7a9e2296862c3a99e40c8f617770851b23fdf15
jw7914 <jw7914@nyu.edu>
2025-09-28 21:57:31 -0400

    inital restful endpoints for hw1

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 # --- TRIGGER DEPENDENCY INJECTION ---
 # By importing the implementation packages, their __init__.py files
 # run and override the factory functions in the protocol packages.
-import gmail_client_impl
-import gmail_message_impl
+import gmail_client_impl  # noqa: F401
+import gmail_message_impl  # noqa: F401
 import mail_client_api
 
 
