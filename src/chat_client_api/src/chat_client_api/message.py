83b6d3bca22fdcccdcc24ab905650d8aa8ee01f4
Evan Hayden Lee <evanhlee@outlook.com>
2025-12-07 18:18:38 -0500

    undo :X

--- src/chat_client_api/src/chat_client_api/message.py
+++ src/chat_client_api/src/chat_client_api/message.py
@@ -38,12 +38,6 @@ class ChatMessage(ABC):
 
     @property
     @abstractmethod
-    def edited_timestamp(self) -> str | None:
-        """Return the date the message was last edited, or None if never edited."""
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
     def content(self) -> str:
         """Return the plain text content of the message."""
         raise NotImplementedError
@@ -73,7 +67,7 @@ class ChatChannel(ABC):
     @property
     @abstractmethod
     def channel_position(self) -> int:
-        """Return the position of the channel in the channel."""
+        """Return the type of the channel."""
         raise NotImplementedError
 
 
